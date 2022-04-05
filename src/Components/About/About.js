70dcd9156f6c94f70c021beb413440895d9ed2bc
Reduwan-Nyx <arnodorain124@gmail.com>
2022-04-05 14:58:58 +0600

    about commited

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import './About.css'
 const About = () => {
     return (
         <div>
-            <h2>this is about</h2>
+            <h6 className='about-text'>Apple Watch is a wearable smartwatch that allows users to accomplish a variety of tasks, including making phone calls, sending text messages and reading email. Apple released the Apple Watch on April 24, 2015.
+
+To function, the Apple Watch must be connected to an iPhone 5 or later running iOS 8.2 or higher, and within a range of about 33 feet. In addition to using a Bluetooth connection, the watch can also connect with an iPhone if it is on the same Wi-Fi network. The Apple Watch iPhone app allows users to change settings, rearrange apps, choose notifications and download new apps from their iPhone. The Apple Watch interface was designed specifically for the wrist, and navigation relies on swiping and tapping. Its Digital Crown is a physical navigation dial located on the side of the watch that magnifies content, scrolls and inputs data.
+
+The watch's Handoff capability allows users to start a task on the Apple Watch and finish it on their iPhone. For example, users cannot compose an email on the watch, but they can ask the Siri digital assistant to write an email and it will transfer the process over to the iPhone. (Siri is present on the Apple Watch but only in text form; she does not speak. To activate Siri the user can either say, “Hey Siri” or hold down the Digital Crown.) Another feature useful to enterprise users is called Glances, delivering quick bursts of information aggregated from apps found on the user's iPhone, which users can customize and access by swiping up on the watch face.
+
+Developers can create or modify existing apps specifically for the Apple Watch with Watch Kit. Most of the current apps from Apple are refactored iPhone apps such as messaging -- however, some native apps have been developed specifically for the watch, including Activity, Workout and Camera Remote. Third-party apps include a social media app for Facebook and apps for airlines and hotels that allow the user to check in through the Apple Watch. Enterprise apps include Evernote, which allows users to dictate information to the watch, and Salesforce1, which delivers real-time information about customer interactions. IT can employ enterprise mobility management products to push corporate emails directly to the Apple Watch.
+
+The standard Apple Watch comes in two case sizes, 38mm or 42mm, with an OLED display with an ambient light sensor to combat glare. Apple offers a premium Apple Watch Edition, which features an 18-karat gold casing and an Apple Watch Sport, which features a durable rubber band and extra-strength Ion-X glass display. Apple Watch's battery lasts 18 hours during general use or 72 hours in Power Reserve mode, which shuts off everything except the clock. A round magnetic charger latches onto the underside of the watch face and uses a USB cord to attach to the same charger as an iPhone.</h6>
         </div>
     );
 };
